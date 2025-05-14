0d4cb08512910fbf263044bb27b4064348eb795c
Sean Yang <sean.yang@thras.io>
2025-05-14 14:17:41 +0800

    Remove .bat from version control

--- run_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Screenshot Saver...
-cd /d "C:\Users\Thrasio\Desktop\Projects\Save_Screentshot_to_PNG"
-"C:\Users\Thrasio\AppData\Local\Programs\Python\Python313\Scripts\streamlit.exe" run app.py
-pause
