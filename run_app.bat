ca989ea666ba3199c7c3875530f6371b8ab906ba
Sean Yang <sean.yang@thras.io>
2025-05-14 14:16:26 +0800

    add a .bat for oneclick local run

---
+++ run_app.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting Screenshot Saver...
+cd /d "C:\Users\Thrasio\Desktop\Projects\Save_Screentshot_to_PNG"
+"C:\Users\Thrasio\AppData\Local\Programs\Python\Python313\Scripts\streamlit.exe" run app.py
+pause
